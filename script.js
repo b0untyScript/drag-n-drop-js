0a17e7fc060bfa7993ab4732f1b493f87c9dcd2e
8olifirenko <8olifirenko@gmail.com>
2024-12-17 19:38:19 +0200

    notes with drag n drop

--- script.js
+++ script.js
@@ -30,3 +30,75 @@ function mouseMove(event) {
 function mouseUp(event) {
     document.removeEventListener("mousemove", mouseMove);
 }
+
+
+// notes with drag n drop
+
+let color = document.querySelector(".color");
+let createBtn = document.querySelector(".createBtn");
+let list = document.querySelector(".list");
+
+createBtn.onclick = () => {
+    let newNote = document.createElement("div");
+    newNote.classList.add("note");
+    newNote.innerHTML = `
+        <span class="close">x</span>
+        <textarea
+        placeholder="Write Content..."
+        rows="10" cols="30"></textarea>
+        `;
+    newNote.style.borderColor = color.value;
+    list.appendChild(newNote);
+};
+
+document.addEventListener("click", (params) => {
+    if (params.target.classList.contains("close")) {
+        params.target.parentNode.remove();
+    }
+});
+
+let cursor = {
+    x: null,
+    y: null
+};
+let note = {
+    dom: null,
+    x: null,
+    y: null
+};
+
+document.addEventListener("mousedown", (params) => {
+    if (params.target.classList.contains("note")) {
+        cursor = {
+            x: params.clientX,
+            y: params.clientY
+        }
+        note = {
+            dom: params.target,
+            x: params.target.getBoundingClientRect().left,
+            y: params.target.getBoundingClientRect().top
+        };
+        console.table(cursor);
+    }
+});
+
+document.addEventListener("mousemove", (params) => {
+    if (note.dom == null) return;
+    let currentCursor = {
+        x: params.clientX,
+        y: params.clientY
+    };
+    let distance = {
+        x: currentCursor.x - cursor.x,
+        y: currentCursor.y - cursor.y
+    };
+    note.dom.style.left = (note.x + distance.x) + "px";
+    note.dom.style.top = (note.y + distance.y) + "px";
+    note.dom.style.cursor = "grab";
+});
+
+document.addEventListener("mouseup", () => {
+    if (note.dom == null) return;
+    note.dom.style.cursor = "auto";
+    note.dom = null;
+});
